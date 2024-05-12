03bcca644fb2f26813c6b697afa33a9dec9862f2
Loyd Cardozo <loyd.cardozo@gmail.com>
2024-05-12 10:06:01 +0530

    first commit

---
+++ lib/pages/main_layout.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int currentPage = 0;
+  final PageController _page = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _page,
+        onPageChanged: ((value){
+          setState(() {
+
+          });
+        }),
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
